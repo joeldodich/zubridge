c1e235103365eb1e3a4e9fb552679b1e4f9249c2
Sam Maister <goosewobbler@protonmail.com>
2025-03-22 11:47:14 +0000

    refactor: standardise tauri useStore with v1

--- apps/tauri/example-store-handlers/src/renderer/hooks/useStore.ts
+++ apps/tauri/example-store-handlers/src/renderer/hooks/useStore.ts
@@ -1,24 +1,5 @@
-import { create } from 'zustand';
-import { rendererZustandBridge } from '@zubridge/tauri';
+import { createUseStore } from '@zubridge/tauri';
 import type { State } from '../../features/index.js';
 
-const initialState: State = {
-  counter: 0,
-};
-
-console.log('useStore: Creating store');
-export const useStore = create<State>((set) => {
-  console.log('useStore: Creating store with initial:', initialState);
-
-  // Create bridge and subscribe to state updates
-  const { handlers } = rendererZustandBridge<State>();
-  handlers.subscribe((newState: State) => {
-    console.log('useStore: Received state update:', newState);
-    set(newState);
-  });
-
-  console.log('useStore: Bridge created');
-  return initialState;
-});
-
-export const { handlers } = rendererZustandBridge<State>();
+// Create a shared store hook for the entire application
+export const useStore = createUseStore<State>();
