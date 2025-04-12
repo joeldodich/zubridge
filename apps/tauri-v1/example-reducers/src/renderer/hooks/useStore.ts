332b40d65802ec0cac4aa8de97eb0dca97838eda
Sam Maister <goosewobbler@protonmail.com>
2025-04-12 15:29:26 +0100

    chore: delete tauri-v1 stuff

--- apps/tauri-v1/example-reducers/src/renderer/hooks/useStore.ts
+++
@@ -1,5 +0,0 @@
-import { createUseStore } from '@zubridge/tauri-v1';
-import type { State } from '../../features/index.js';
-
-// Create a shared store hook for the entire application
-export const useStore = createUseStore<State>();
