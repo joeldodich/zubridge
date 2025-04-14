8cbc13cd4c67273478816f3adefa76217dab54b4
Sam Maister <goosewobbler@protonmail.com>
2025-04-14 12:41:19 +0100

    chore(tauri-v1-example): delete unused

--- apps/tauri-v1-example/src/renderer/hooks/useStore.ts
+++
@@ -1,5 +0,0 @@
-import { createUseStore } from '@zubridge/electron';
-import type { State } from '../../types/state';
-
-// Create a shared store hook for the entire application
-export const useStore = createUseStore<State>();
