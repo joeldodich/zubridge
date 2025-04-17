a1dc94f90dd1494e22b2f7f7135dc55ea4bc5dc1
Sam Maister <goosewobbler@protonmail.com>
2025-04-17 16:31:35 +0100

    fix(electron-example): update broken type import

--- apps/electron-example/src/renderer/hooks/useStore.ts
+++ apps/electron-example/src/renderer/hooks/useStore.ts
@@ -1,5 +1,5 @@
 import { createUseStore } from '@zubridge/electron';
-import type { State } from '../../types/state';
+import type { State } from '../../types/index.js';
 
 // Create a shared store hook for the entire application
 export const useStore = createUseStore<State>();
