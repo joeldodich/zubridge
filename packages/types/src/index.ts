73d63c0cda849cc8914d5a36d8d9048ef95c7139
Sam Maister <goosewobbler@protonmail.com>
2025-04-28 20:10:39 +0100

    refactor(types): update wrapper / webcontents handling

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -74,12 +74,12 @@ export interface ZustandBridge extends BaseBridge<number> {
   subscribe: (wrappers: [WebContentsWrapper, ...WebContentsWrapper[]]) => { unsubscribe: () => void };
 }
 
-export type WrappersOrWebContents = [WebContentsWrapper | WebContents, ...(WebContentsWrapper | WebContents)[]];
+export type WrapperOrWebContents = WebContentsWrapper | WebContents;
 
 // The function type for initializing the bridge
 export type MainZustandBridge = <S extends AnyState, Store extends StoreApi<S>>(
   store: Store,
-  wrappers: WrappersOrWebContents,
+  wrappers: WrapperOrWebContents,
   options?: MainZustandBridgeOpts<S>,
 ) => ZustandBridge;
 
@@ -117,9 +117,9 @@ export interface StateManager<State> {
 
 // Base interface for backend bridges across platforms
 export interface BackendBridge<WindowId> extends BaseBridge<WindowId> {
-  subscribe: (windows: WrappersOrWebContents) => {
+  subscribe: (windows: WrapperOrWebContents[]) => {
     unsubscribe: () => void;
   };
-  unsubscribe: (windows?: WrappersOrWebContents) => void;
+  unsubscribe: (windows?: WrapperOrWebContents[]) => void;
   destroy: () => void;
 }
